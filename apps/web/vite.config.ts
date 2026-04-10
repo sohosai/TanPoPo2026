f7d4c4893fabb02a3c5adc5fdecd8965b23e582c
Tomoro0726 <s.tomoro0726@gmail.com~>
2026-04-10 19:55:12 +0900

    delete tailwindcss

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,4 @@
 import { reactRouter } from '@react-router/dev/vite';
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
@@ -8,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const webPort = Number.isNaN(parsedWebPort) ? 5173 : parsedWebPort;
 
   return {
-    plugins: [tailwindcss(), reactRouter()],
+    plugins: [reactRouter()],
     resolve: {
       tsconfigPaths: true,
     },
